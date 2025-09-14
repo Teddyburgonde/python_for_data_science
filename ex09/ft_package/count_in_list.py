7f6b7dde810733411726a2e2cfc03a931c27526b
Teddy Bandama <tebandam@1G6.42angouleme.fr>
2025-09-14 16:44:05 +0200

    finished ex09

--- ex09/ft_package/count_in_list.py
+++
@@ -1,5 +0,0 @@
-def count_in_list(lst, item):
-    """
-    Count how many times 'item' appears in 'lst'.
-    """
-    return lst.count(item)
---
+++ ex09/src/ft_package/count_in_list.py
@@ -0,0 +1,15 @@
+def count_in_list(lst, item):
+    """
+    Count how many times a specific item appears in a list.
+
+    This function iterates over the given list and returns the total number
+    of occurrences of the specified item.
+
+    :param lst:
+        A list of elements in which to search.
+    :param item:
+        The element whose occurrences will be counted.
+    :returns:
+        An integer representing the number of times ``item`` appears in ``lst``.
+    """
+    return lst.count(item)
